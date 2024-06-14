03e14ed6dca3f5c61f728d81933e540fbfcd72aa
LoanF <loanfrancois1@gmail.com>
2024-06-14 11:38:07 +0200

    fix notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,7 @@
 import closesvg from '../assets/close.png'
 export default function Notification({ message = "message", type = "info", onClose }) {
     return (
-        <div className={`absolute z-20 flex space-x-3 animate-[notification_1s_ease-in-out] bottom-10 right-5 px-5 py-3 rounded-xl items-center ${type}`}>
+        <div className={`fixed z-20 flex space-x-3 animate-[notification_1s_ease-in-out] bottom-10 right-5 px-5 py-3 rounded-xl items-center ${type}`}>
             <span>{message}</span>
             <a className='cursor-pointer' onClick={onClose}>
                 <img className='w-[20px]' src={closesvg} alt="" />
