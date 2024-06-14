2a997def171af0f3673a30b041b1f9f457976116
lololokoko452 <mano.raichon@orange.fr>
2024-06-14 13:56:14 +0200

    Notification changement disposition

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,7 @@
 import closesvg from '../assets/close.png'
 export default function Notification({ message = "message", type = "info", onClose }) {
     return (
-        <div className={`flex space-x-3 animate-[notification_1s_ease-in-out] px-5 py-3 rounded-xl items-center ${type}`}>
+        <div className={`flex space-x-3 animate-[notification_1s_ease-in-out] px-5 py-3 rounded-xl items-center justify-between ${type}`}>
             <span>{message}</span>
             <a className='cursor-pointer' onClick={onClose}>
                 <img className='w-[20px]' src={closesvg} alt="" />
